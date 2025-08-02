786a6d2ac3a7616c0efdace91546baebf8eaedf9
Jeremy <jeremy.geneste@gmail.com>
2025-08-02 14:22:45 +0200

    add roles, chat history and loop to keep chating

--- app.py
+++ app.py
@@ -6,17 +6,38 @@ load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
 
-api_url = "https://api.openai.com/v1/responses"
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {API_KEY}" 
-}
-body = {
-    "model": "gpt-4o-mini",
-    "input": "write a haiku about ai"
-  }
+history = []
 
-response = requests.post(api_url, headers=headers, json=body)
-response_data = response.json()
+while True:
+    user_input = input("Your prompt or question : ")
 
-print(response_data)
+    if user_input == "":
+        break
+
+    history.append({
+        "role": "user",
+        "content": user_input
+    })
+
+    api_url = "https://api.openai.com/v1/responses"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {API_KEY}" 
+    }
+    body = {
+        "model": "gpt-4o-mini",
+        "instructions": "Parle comme si tu es Napoléon Bonaparte",
+        "input": history
+    }
+
+    response = requests.post(api_url, headers=headers, json=body)
+    response_data = response.json()
+
+    text = response_data['output'][0]["content"][0]["text"]
+
+    print(text)
+
+    history.append({
+        "role": "assistant",
+        "content": text
+    })
